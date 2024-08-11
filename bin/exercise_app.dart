d24865e94b7cb2ad9c2af6ea8128cc7b233327e2
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-08-11 14:08:58 -0500

    fifth exercise

--- bin/exercise_app.dart
+++ bin/exercise_app.dart
@@ -1,10 +1,10 @@
 // import 'package:exercise_app/exercise1.dart';
 // import 'package:exercise_app/fibonacci.dart';
 // import 'package:exercise_app/prime_number.dart';
-
-import 'package:exercise_app/polygon_area.dart';
+// import 'package:exercise_app/polygon_area.dart';
+import 'package:exercise_app/reverse_chain.dart';
 
 void main(List<String> args) {
-  final polygonArea = Figures.polygonArea(25, 3.44);
-  print(polygonArea);
+  final reverseString = ReverseChain.reverseChain('nodlehs otceyorp');
+  print(reverseString);
 }
