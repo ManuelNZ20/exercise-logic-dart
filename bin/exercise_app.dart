4be250c71c24a9f8df48ebe3277bf8420e404de6
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2024-08-11 13:32:29 -0500

    fourth exercise, polygon area

--- bin/exercise_app.dart
+++ bin/exercise_app.dart
@@ -1,9 +1,10 @@
 // import 'package:exercise_app/exercise1.dart';
 // import 'package:exercise_app/fibonacci.dart';
+// import 'package:exercise_app/prime_number.dart';
 
-import 'package:exercise_app/prime_number.dart';
+import 'package:exercise_app/polygon_area.dart';
 
 void main(List<String> args) {
-  final isPrime = PrimeNumber.isPrime(13);
-  print(isPrime);
+  final polygonArea = Figures.polygonArea(25, 3.44);
+  print(polygonArea);
 }
